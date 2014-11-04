9396a29db429d69b85f8713167321a4cd84c65f2
Lukas Schwab <lukas.schwab@gmail.com>
2014-11-04 13:25:14 -0800

    Updated localized-analysis.py

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ My 2014-2015 IB Math HL Internal Assessment
 Wikipedia network topography analysis, written in Python. No title yet.
 
 Dataset thanks to [Henry Haselgrove](http://haselgrove.id.au/wikipedia.htm)––you can get it at that link.
+
+Coming soon here:
+
++ ToDo list
++ File index
--- localized-analysis.py
+++ localized-analysis.py
@@ -4,39 +4,37 @@ from unidecode import unidecode
 # I want a -l command to give the information on all the nodes involved
 # Otherwise, just the numbers resulting from analysis
 
-# Randomly select a node
-lineno = random.randint(1,5706070)
-
 # Gather its info
-linestr = linecache.getline('links-simple-sorted.txt',lineno)
-[origin, dest] = linestr.split(':')
-dest = dest[1:-1]
-dest = dest.split(' ')
-dest[:] = [int(x) for x in dest]
+finallist = [None] * 5716809
+with open('links-simple-sorted.txt', 'r') as src:
+	for line in src:
+		[oNode, dNode] = line.split(':')
+		finallist[int(oNode)] = dNode.rstrip('\n')[1:]
+
+# Randomly select a node
+oNode = random.randint(1,5716809)
+dNode = finallist[oNode].split(' ')
 
 # Gather the same info for each of the neighbors
-# Indexed by twoSteps[neighborIndex1][neigborIndex2]
-twoSteps = [None] * len(dest)
-counter = 0
-for neighbor in dest:
-	linestr = linecache.getline('links-simple-sorted.txt',lineno)
-	[waste, nOut] = linestr.split(':')
-	nOut = nOut[1:-1]
-	nOut = nOut.split(' ')
-	nOut[:] = [int(x) for x in nOut]
-	twoSteps[counter] = nOut
-	counter+=1
+# Make this a function, can do recursive calls to check multiple depths?
+twoSteps = ''
+for neighbor in dNode:
+	twoSteps += finallist[int(neighbor)]
+twoSteps = twoSteps.split(' ')
 
-print 'Node analyzed: ' + origin
-print 'Connectivity: ' + str(len(dest))
+# Have to find the number of overlaps
+oneANDtwo = 0
+for firstStep in dNode:
+	oneANDtwo += twoSteps.count(firstStep)
 
-# CLUSTERING COEFFICIENT
-ai = len(dest)
-Ei = float(0)
-# Nested loop to be able to check links between neighbors
-for x in twoSteps:
-	tris = list(set(x) & set(dest)) # Network triangles
-	Ei += len(tris)
-# Calculate the coefficient
-clCoeff = (2*Ei)/(ai*(ai-1))
-print 'Clustering Coefficient: ' + str(clCoeff)
+print 'Node analyzed: ' + str(oNode)
+print 'Connectivity: ' + str(len(dNode))
+print 'Two-step connectivity: ' + str(len(twoSteps))
+
+if len(dNode) > 1:
+	# CLUSTERING COEFFICIENT
+	ai = float(len(dNode))
+	Ei = float(oneANDtwo)
+	# Calculate the coefficient
+	clCoeff = (2*Ei)/(ai*(ai-1))
+	print 'Clustering Coefficient: ' + str(clCoeff)
