1d67f36a4c1a5de227791a665f87f2cb0b960214
Lukas Schwab <lukas.schwab@gmail.com>
2014-11-04 09:19:34 -0800

    Added usage notes for listmake.py

--- listmake.py
+++ listmake.py
@@ -1,3 +1,8 @@
+# Creates list finallist
+# For index i
+# finallist[i] is the links out, in the form of a string separated by spaces:
+# 	finallist[2] = '3 747213 1664968 1691047 4095634 5535664'
+
 finallist = [None] * 5716809
 
 with open('links-simple-sorted.txt', 'r') as src:
