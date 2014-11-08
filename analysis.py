03dc0e5d0adecf18bc83abfb979bb340f1336059
Lukas Schwab <lukas.schwab@gmail.com>
2014-11-08 15:34:14 -0800

    Only analyze non-redirect pages

--- analysis.py
+++ analysis.py
@@ -4,10 +4,13 @@ from unidecode import unidecode
 from localized_analysis import clCoeff
 
 # Process links into list
+notredirects = []
 finallist = [None] * 5716809
 with open('links-simple-sorted.txt', 'r') as src:
 	for line in src:
 		[oNode, dNode] = line.split(':')
+		if len(dNode.rstrip('\n').split(' ')) > 2:
+			notredirects += [int(oNode)]
 		finallist[int(oNode)] = dNode.rstrip('\n')[1:]
 
 ##########################################################################################
@@ -44,6 +47,6 @@ with open('links-simple-sorted.txt', 'r') as src:
 ##########################################################################################
 # Clustering coefficient for every node
 
-for node in range (1,5716810):
-	test = clCoeff(node, finallist)
-	print node
+for node in notredirects:
+	test = clCoeff(int(node), finallist)
+	print str(node) + ' / 5716809'
