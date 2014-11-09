181e0432ecf2477e2243112651c92661aff8e2b5
Lukas Schwab <lukas.schwab@gmail.com>
2014-11-08 22:14:59 -0800

    Only evaluate some nodes, misc.

--- analysis.py
+++ analysis.py
@@ -13,40 +13,8 @@ with open('links-simple-sorted.txt', 'r') as src:
 			notredirects += [int(oNode)]
 		finallist[int(oNode)] = dNode.rstrip('\n')[1:]
 
-##########################################################################################
-# Analyze basic properties of a particular node (randomly selected) 
-
-# Get a random node, and pull that line from the links doc; want this to be an option
-# oNode = random.randint(1,5706070)
-# dNode = finallist[oNode]
-# dNode = dNode.split(' ')
-# Translate these into titles and print the result
-# oname = linecache.getline('titles-sorted.txt',int(oNode))
-# oname = oname[:-1] # Gets rid of the trailing newline
-# print '\nORIGIN NODE: ' + oname + '\n'
-# print 'DESTINATION NODES:'
-# for thisnum in dNode:
-# 	dname = linecache.getline('titles-sorted.txt',int(thisnum))[:-1]
-# 	print '     ' + dname
-# print '\n'
-
-##########################################################################################
-# Connectivity across the whole network
-
-# Calculate average number of 1st-order connections
-# totalConn = 0
-# linksout = []
-# for node in finallist:
-# 	if type(node) != type(None): # If the oNode has outlinks
-# 		totalConn += len(node.split(' '))
-# 		linksout += [len(node.split(' '))]
-# 	else:
-# 		linksout += [0]
-# avgConn = float(totalConn/5716809)
-
-##########################################################################################
-# Clustering coefficient for every node
-
-for node in notredirects:
+node = 1
+while node <= len(notredirects):
 	test = clCoeff(int(node), finallist)
-	print str(node) + ' / 5716809'
+	print str(node) + ' / ' + str(len(notredirects)) + ':     ' + str(test)
+	node += 100
